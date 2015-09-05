7dd94ba771c3886517e67d6b917bff312ed5ab13
Jose Gomez Castaño <jgcasta@gmail.com>
2015-09-05 13:05:01 +0200

    Activities interacction

--- app/src/main/java/es/astroide/metoritedetector/MainActivity.java
+++ app/src/main/java/es/astroide/metoritedetector/MainActivity.java
@@ -1,18 +1,34 @@
 package es.astroide.metoritedetector;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View boton = findViewById(R.id.btnStart);
+        boton.setOnClickListener(this);
     }
+
+
+    public void onClick(View vista) {
+
+        if(vista.getId()==findViewById(R.id.btnStart).getId()){
+            Intent i = new Intent(this,MedicionesActivity.class);
+            startActivity(i);
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
